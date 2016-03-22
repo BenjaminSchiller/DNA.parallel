8d1c36a66a94828ac40ffaa746061347914aefd6
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2016-03-22 14:50:32 +0100

    extends connectivity test

--- java/src/dna/parallel/test/TestConnectivity.java
+++ java/src/dna/parallel/test/TestConnectivity.java
@@ -8,7 +8,9 @@ import dna.graph.generators.random.RandomGraph;
 import dna.metrics.Metric;
 import dna.metrics.MetricNotApplicableException;
 import dna.metrics.connectivity.WCSimpleR;
+import dna.metrics.connectivity.WCSimpleU;
 import dna.plot.Plotting;
+import dna.plot.PlottingConfig.PlotFlag;
 import dna.series.Series;
 import dna.series.data.SeriesData;
 import dna.updates.generators.BatchGenerator;
@@ -21,14 +23,15 @@ public class TestConnectivity {
 			MetricNotApplicableException, InterruptedException {
 		Execute.exec("rm -r data/test/");
 		Config.zipRuns();
-		GraphGenerator gg = new RandomGraph(GDS.undirected(), 100, 0);
-		BatchGenerator bg = new RandomBatch(0, 0, 5, 0);
-		Metric[] metrics = new Metric[] { new WCSimpleR(), new WCSimpleR() };
+		GraphGenerator gg = new RandomGraph(GDS.undirected(), 100, 500);
+		BatchGenerator bg = new RandomBatch(0, 1, 0, 2);
+		Metric[] metrics = new Metric[] { new WCSimpleR(), new WCSimpleU() };
 
 		Series s = new Series(gg, bg, metrics, "data/test/connectivity/",
 				"name");
-		SeriesData sd = s.generateRuns(0, 0, 20);
-		Plotting.plot(sd, "data/test/connectivity-plots/");
+		SeriesData sd = s.generateRuns(0, 0, 50);
+		Plotting.plot(sd, "data/test/connectivity-plots/",
+				PlotFlag.plotMetricValues);
 		Execute.exec("open data/test/connectivity-plots/");
 	}
 }
